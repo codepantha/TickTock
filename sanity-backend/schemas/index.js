53c818c73905c10f07284d52a0d8103d103edba3
codepanther <pezechigozirim@gmail.com>
2023-01-10 18:54:45 +0100

    Add post and comment Schema

--- sanity-backend/schemas/index.js
+++ sanity-backend/schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import user from './user'
+import post from './post'
+import postedBy from './postedBy'
+import comment from './comment'
+
+export const schemaTypes = [user, post, postedBy, comment]
