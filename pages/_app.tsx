0e08bb30f8e29de08e9ebb87a05bb44b3206e89f
codepanther <pezechigozirim@gmail.com>
2023-01-15 19:26:09 +0100

    Adjust display width for screen sizes 1200px and above

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,13 +16,15 @@ export const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN!}>
-      <Navbar />
-      <div className="flex gap-6 md:gap-20">
-        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          <Sidebar />
-        </div>
-        <div className="mt-4 flex flex-1 flex-col gap-10 overflow-auto h-[88vh] videos">
-          <Component {...pageProps} />
+      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+        <Navbar />
+        <div className="flex gap-6 md:gap-20">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-1 flex-col gap-10 overflow-auto h-[88vh] videos">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </GoogleOAuthProvider>
