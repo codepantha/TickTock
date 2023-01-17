f130a17e363f4410666e1652ae025fb9ebb34e61
codepanther <pezechigozirim@gmail.com>
2023-01-17 10:44:19 +0100

    Add like and unlike functionality

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -4,10 +4,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { MdOutlineCancel } from 'react-icons/md';
 import axios from 'axios';
+import { BsFillPauseFill, BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeOff, HiVolumeUp } from 'react-icons/hi';
+import { GoVerified } from 'react-icons/go';
 
 import { BASE_URL } from '../../utils';
-import { BsFillPlayFill } from 'react-icons/bs';
-import { HiVolumeOff, HiVolumeUp } from 'react-icons/hi';
+import useAuthStore from '../../store/authStore';
+import LikeButton from '../../components/LikeButton';
+import Comments from '../../components/Comments';
 
 interface IProps {
   postDetails: Video;
@@ -17,19 +21,23 @@ const Detail = ({ postDetails }: IProps) => {
   const [post, setPost] = useState(postDetails);
   const [playing, setPlaying] = useState<boolean>(false);
   const [isMuted, setIsMuted] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
+
+  const router = useRouter();
+  const { userProfile }: any = useAuthStore();
 
   if (!post) return;
 
   const onVideoClick = () => {
     if (!playing) {
-      videoRef.current?.play()
+      videoRef.current?.play();
       setPlaying(true);
     } else {
       videoRef.current?.pause();
       setPlaying(false);
     }
-  }
+  };
 
   const handleVideoAudio = () => {
     if (!isMuted) {
@@ -40,10 +48,22 @@ const Detail = ({ postDetails }: IProps) => {
     } else {
       if (videoRef.current) {
         videoRef.current.muted = false;
-        setIsMuted(false)
+        setIsMuted(false);
       }
     }
-  }
+  };
+
+  const handleLike = async (like: boolean) => {
+    if (userProfile) {
+      const { data } = await axios.put(`${BASE_URL}/api/like`, {
+        userId: userProfile._id,
+        postId: post._id,
+        like
+      });
+
+      setPost({ ...post, likes: data.likes })
+    }
+  };
 
   return (
     <div
@@ -57,11 +77,18 @@ const Detail = ({ postDetails }: IProps) => {
       >
         <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
           <p>
-            <MdOutlineCancel className="text-white text-[35px]" />
+            <MdOutlineCancel
+              onClick={() => router.back()}
+              className="text-white text-[35px] cursor-pointer"
+            />
           </p>
         </div>
 
-        <div className="relative">
+        <div
+          className="relative"
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+        >
           <div className="lg:h-[100vh] h-[60vh]">
             <video
               ref={videoRef}
@@ -72,26 +99,93 @@ const Detail = ({ postDetails }: IProps) => {
           </div>
 
           <div className="absolute top-[45%] left-[45%]">
-            {!playing && (
+            {playing ? (
+              <button
+                onClick={onVideoClick}
+                className={`text-white text-6xl lg:text-8xl ${
+                  !isHovering && 'hidden'
+                }`}
+              >
+                <BsFillPauseFill />
+              </button>
+            ) : (
               <button onClick={onVideoClick}>
-                <BsFillPlayFill className="text-white text-6xl lg:text-8xl" />
+                <BsFillPlayFill
+                  className={`text-white text-6xl lg:text-8xl ${
+                    !isHovering && 'hidden'
+                  }`}
+                />
               </button>
             )}
           </div>
         </div>
 
-        <div className="absolute bottom-5 lg:bottom-10
+        <div
+          className="absolute bottom-5 lg:bottom-10
           right-5 lg:right-10 cursor-pointer"
         >
           {isMuted ? (
-              <button onClick={handleVideoAudio} className="buttonControls" >
-                <HiVolumeOff />
-              </button>
-            ) : (
-              <button onClick={handleVideoAudio} className="buttonControls">
-                <HiVolumeUp />
-              </button>
+            <button onClick={handleVideoAudio} className="buttonControls">
+              <HiVolumeOff />
+            </button>
+          ) : (
+            <button onClick={handleVideoAudio} className="buttonControls">
+              <HiVolumeUp />
+            </button>
+          )}
+        </div>
+      </div>
+
+      <div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+        <div className="lg:mt-20 mt-10">
+          <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+            <div className="ml-4 md:w-20 md:h-20 w-16 h-16">
+              <Link href="/">
+                <>
+                  <Image
+                    width={62}
+                    height={62}
+                    className="rounded-full"
+                    src={post.postedBy.image}
+                    alt="profile photo"
+                    layout="responsive"
+                  />
+                </>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <div className="mt-3 flex flex-col gap-2">
+                  <p
+                    className="flex gap-2 items-center md:text-md
+                      font-bold text-primary"
+                  >
+                    {post.postedBy.userName} {` `}
+                    <GoVerified className="text-blue-400 text-md" />
+                  </p>
+                  <p
+                    className="capitalize font-medium text-sm
+                  text-gray-500 hidden md:block"
+                  >
+                    {post.postedBy.userName}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
+
+          <p className="px-10 text-lg text-gray-600">{post.caption}</p>
+
+          <div className="mt-10 px-10">
+            {userProfile && (
+              <LikeButton
+                likes={post.likes}
+                handleLike={() => handleLike(true)}
+                handleDislike={() => handleLike(false)}
+              />
             )}
+          </div>
+          <Comments />
         </div>
       </div>
     </div>
