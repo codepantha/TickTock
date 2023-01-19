59a0038ce5606449afe8d44e8eecb897afbb41a8
codepanther <pezechigozirim@gmail.com>
2023-01-19 05:33:34 +0100

    Change next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
   reactStrictMode: true,
   images: {
     domains: ['c8.alamy.com', 'lh3.googleusercontent.com']
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
